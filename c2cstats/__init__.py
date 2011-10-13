b44e140c3ea1b033b215d7b36829ded229da0482
Simon <contact@saimon.org>
2011-10-14 00:33:26 +0200

    use skeleton for html/css

--- c2cstats/__init__.py
+++ c2cstats/__init__.py
@@ -3,6 +3,10 @@
 
 import os
 import urlparse
+import locale
+locale.setlocale(locale.LC_ALL, '')
+
+from datetime import datetime
 
 from c2cstats.parser import Outings, ParserError
 from c2cstats.plots import Plots
@@ -36,19 +40,25 @@ def show_user_stats(user_id):
 
     data_dir = app.config['IMG_DIR']
 
+    context = {
+        'img_url': app.config['IMG_URL'],
+        'user_id': user_id,
+        }
+
     if user_id not in os.listdir(data_dir):
         try:
             data = Outings(user_id)
         except ParserError:
             flash('Error while loading page', 'error')
+            return redirect(url_for('index'))
 
         plots = Plots(data, data_dir)
         plots.plot_all()
 
-    context = {
-        'img_url': app.config['IMG_URL'],
-        'user_id': user_id,
-        }
+        context['nboutings'] = data.nboutings
+
+    d = datetime.now()
+    context['date_generated'] = unicode(d.strftime('%d %B %Y à %X'), 'utf-8')
 
     context['files'] = [f for f in os.listdir(os.path.join(data_dir, user_id))
                         if os.path.splitext(f)[1] in app.config['IMG_EXT']]
