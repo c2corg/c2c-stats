a1bcb391fecc3908e04c2c8fc6d51fef7fb8c70a
Simon <contact@saimon.org>
2011-11-02 00:13:23 +0100

    use generate_json in the flask app

--- c2cstats/__init__.py
+++ c2cstats/__init__.py
@@ -10,8 +10,7 @@ locale.setlocale(locale.LC_ALL, '')
 from datetime import datetime
 
 from c2cstats.parser import Outings, ParserError
-from c2cstats.plots import Plots
-from c2cstats.writer import Writer
+from c2cstats.generators import generate_json
 
 from flask import Flask, request, g, redirect, url_for, \
      render_template, flash
@@ -20,7 +19,6 @@ from flask import Flask, request, g, redirect, url_for, \
 SECRET_KEY = 'development key'
 IMG_DIR = '_output'
 IMG_URL = '/images'
-IMG_EXT = ['.png']
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -40,41 +38,26 @@ def show_user_stats(user_id):
     user_id = str(user_id)
 
     data_dir = app.config['IMG_DIR']
+    json_file = os.path.join(data_dir, user_id + '.json')
 
     context = {
-        'img_url': app.config['IMG_URL'],
+        'json_url': app.config['IMG_URL'] + '/' + user_id + '.json',
         'user_id': user_id,
         }
 
-    user_path = os.path.join(data_dir, user_id)
-    index_file = os.path.join(user_path, 'index.html')
-
-    if os.path.isfile(index_file):
-        with codecs.open(index_file, encoding='utf-8', mode='r') as f:
-            page = f.read()
-    else:
+    if not os.path.isfile(json_file):
         try:
-            data = Outings(user_id)
+            generate_json(user_id, json_file)
         except ParserError:
             flash('Error while loading page', 'error')
             return redirect(url_for('index'))
 
-        plots = Plots(data, data_dir)
-        plots.plot_all()
+    # context['nboutings'] = data.nboutings
 
-        context['nboutings'] = data.nboutings
+    d = datetime.now()
+    context['date_generated'] = unicode(d.strftime('%d %B %Y à %X'), 'utf-8')
 
-        d = datetime.now()
-        context['date_generated'] = unicode(d.strftime('%d %B %Y à %X'), 'utf-8')
-
-        context['files'] = [f for f in os.listdir(user_path)
-                            if os.path.splitext(f)[1] in app.config['IMG_EXT']]
-
-        page = render_template('user.html', **context)
-        with codecs.open(index_file, encoding='utf-8', mode='w') as f:
-            f.write(page)
-
-    return page
+    return render_template('user.html', **context)
 
 
 @app.route('/query', methods=['POST'])
