7a1662e48283280c6f12585821d4b005ce9850fe
Simon <contact@saimon.org>
2011-11-07 23:51:26 +0100

    new plot: cotation escalade

--- c2cstats/generators.py
+++ c2cstats/generators.py
@@ -135,14 +135,16 @@ class Escalade(Generator):
                 '6c+', '7a', '7a+', '7b', '7b+', '7c', '7c+', '8a')
     # '2', '8a+', '8b', '8b+', '8c', '8c+', '9a', '9a+', '9b', '9b+'
 
-    XLABEL = u'Cotation escalade'
-
     def __init__(self, *args, **kwargs):
         Generator.__init__(self, *args, **kwargs)
 
     @property
     def cotation(self):
-        return self.cot_libre
+        return {
+            'title': u'Cotation escalade',
+            'labels': self.COTATION_REF,
+            'values': self.cot_libre
+            }
 
     @property
     def cot_libre(self):
