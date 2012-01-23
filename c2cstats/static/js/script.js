b9e76b052c25eb397451d829fc3867a5157f4887
Simon <contact@saimon.org>
2012-01-23 13:47:34 +0100

    try plots with g.raphael

--- c2cstats/static/js/script.js
+++ c2cstats/static/js/script.js
@@ -5,24 +5,35 @@
 barWidth = 0.6;
 
 function plot_pie(raw, chartdiv) {
-    var data = [];
+    var data = {labels: [], values: []};
     $.each(raw.values, function(index, value) {
-        data.push({label: value[0],  data: value[1]});
+        data.labels.push(value[0]+' ('+value[1]+')');
+        data.values.push(value[1]);
     });
 
-    $(chartdiv).before('<h2 class="chart_title">'+raw.title+'</h2>');
-    $.plot($(chartdiv), data, {
-	series: {
-            pie: { show: true },
-        },
-        legend: { show: false },
-        grid: {
-            hoverable: true,
-            clickable: true
+    $('#'+chartdiv).before('<h2 class="chart_title">'+raw.title+'</h2>');
+
+    var r = Raphael(chartdiv);
+    var pie = r.piechart(150, 150, 120, data.values, { legend: data.labels, legendpos: "east"});
+    // r.text(320, 100, "Interactive Pie Chart").attr({ font: "20px sans-serif" });
+    pie.hover(function () {
+        this.sector.stop();
+        this.sector.scale(1.1, 1.1, this.cx, this.cy);
+        if (this.label) {
+            this.label[0].stop();
+            this.label[0].attr({ r: 7.5 });
+            this.label[1].attr({ "font-weight": 800 });
+        }
+    }, function () {
+        this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+        if (this.label) {
+            this.label[0].animate({ r: 5 }, 500, "bounce");
+            this.label[1].attr({ "font-weight": 400 });
         }
     });
 }
 
+/*
 function plot_cotation(raw, chartdiv) {
     var data = [];
     $.each(raw.values, function(index, value) {
@@ -88,6 +99,7 @@ function plot_cotation_globale_per_activity(data) {
     });
 
 }
+*/
 
 function renderplot(data) {
     if (data === null || data.length === 0) {
@@ -103,8 +115,10 @@ function renderplot(data) {
     $("#total_time").text(data.total_time);
     $("#origin-link").attr({href: data.url});
 
-    plot_pie(data.global.activities, '#chart_activities');
-    plot_pie(data.global.area, '#chart_area');
+    plot_pie(data.global.activities, 'chart_activities');
+    plot_pie(data.global.area, 'chart_area');
+
+    /*
     // plot_cotation(data.global.cotation_globale, '#chart_cot_globale');
     plot_cotation_globale_per_activity(data.global.cotation_per_activity);
 
@@ -114,4 +128,5 @@ function renderplot(data) {
             plot_cotation(data[value].cotation, '#cotation_'+value);
         }
     });
+    */
 }
