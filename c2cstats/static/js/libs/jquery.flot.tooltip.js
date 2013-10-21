0bffaea2b981907a08ce0d381c0441788886bac0
Simon Conseil <contact@saimon.org>
2013-10-21 23:39:27 +0200

    Update js libs (flot 0.8.1, flot.tooltip, sparkline).

--- c2cstats/static/js/libs/jquery.flot.tooltip.js
+++ c2cstats/static/js/libs/jquery.flot.tooltip.js
@@ -2,14 +2,13 @@
  * jquery.flot.tooltip
  * 
  * description: easy-to-use tooltips for Flot charts
- * version: 0.6
+ * version: 0.6.2
  * author: Krzysztof Urbas @krzysu [myviews.pl]
  * website: https://github.com/krzysu/flot.tooltip
  * 
- * build on 2013-01-07
+ * build on 2013-09-30
  * released under MIT License, 2012
-*/
-
+*/ 
 (function ($) {
 
     // plugin options, default values
@@ -47,12 +46,12 @@
 
     // main plugin function
     FlotTooltip.prototype.init = function(plot) {
-            
+
         var that = this;
 
         plot.hooks.bindEvents.push(function (plot, eventHolder) {
 
-            // get plot options            
+            // get plot options
             that.plotOptions = plot.getOptions();
 
             // if not enabled return
@@ -63,43 +62,52 @@
 
             // create tooltip DOM element
             var $tip = that.getDomElement();
-            
+
             // bind event
-            $( plot.getPlaceholder() ).bind("plothover", function (event, pos, item) {
-                if (item) {                    
-                    var tipText;
-                    
-                    // convert tooltip content template to real tipText
-                    tipText = that.stringFormat(that.tooltipOptions.content, item);
-                    
-                    $tip.html( tipText )
-                        .css({
-                            left: that.tipPosition.x + that.tooltipOptions.shifts.x,
-                            top: that.tipPosition.y + that.tooltipOptions.shifts.y
-                        })
-                        .show();
-                
-                    // run callback
-                    if(typeof that.tooltipOptions.onHover === 'function') {
-                        that.tooltipOptions.onHover(item, $tip);
-                    }
-                }
-                else {
-                    $tip.hide().html('');
-                }
-            });
-            
-            eventHolder.mousemove( function(e) {
-                var pos = {};
-                pos.x = e.pageX;
-                pos.y = e.pageY;
-                that.updateTooltipPosition(pos);
-            });
+            $( plot.getPlaceholder() ).bind("plothover", plothover);
+			
+			$(eventHolder).bind('mousemove', mouseMove);
+ 
         });
+		plot.hooks.shutdown.push(function (plot, eventHolder){
+			$(plot.getPlaceholder()).unbind("plothover", plothover);
+			$(eventHolder).unbind("mousemove", mouseMove);
+		});
+        function mouseMove(e){ 
+            var pos = {};
+            pos.x = e.pageX;
+            pos.y = e.pageY;
+            that.updateTooltipPosition(pos);
+        }
+		function plothover(event, pos, item) {
+			var $tip = that.getDomElement();
+            if (item) {
+                var tipText;
+
+                // convert tooltip content template to real tipText
+                tipText = that.stringFormat(that.tooltipOptions.content, item);
+
+                $tip.html( tipText );
+                that.updateTooltipPosition({ x: pos.pageX, y: pos.pageY });
+                $tip.css({
+                        left: that.tipPosition.x + that.tooltipOptions.shifts.x,
+                        top: that.tipPosition.y + that.tooltipOptions.shifts.y
+                    })
+                    .show();
+
+                // run callback
+                if(typeof that.tooltipOptions.onHover === 'function') {
+                    that.tooltipOptions.onHover(item, $tip);
+                }
+            }
+            else {
+                $tip.hide().html('');
+            }
+        }
     };
 
     /**
-     * get or create tooltip DOM element 
+     * get or create tooltip DOM element
      * @return jQuery object
      */
     FlotTooltip.prototype.getDomElement = function() {
@@ -111,7 +119,7 @@
         else {
             $tip = $('<div />').attr('id', 'flotTip');
             $tip.appendTo('body').hide().css({position: 'absolute'});
-        
+
             if(this.tooltipOptions.defaultTheme) {
                 $tip.css({
                     'background': '#fff',
@@ -119,7 +127,9 @@
                     'padding': '0.4em 0.6em',
                     'border-radius': '0.5em',
                     'font-size': '0.8em',
-                    'border': '1px solid #111'
+                    'border': '1px solid #111',
+                    'display': 'none',
+                    'white-space': 'nowrap'
                 });
             }
         }
@@ -129,10 +139,18 @@
 
     // as the name says
     FlotTooltip.prototype.updateTooltipPosition = function(pos) {
+        var totalTipWidth = $("#flotTip").outerWidth() + this.tooltipOptions.shifts.x;
+        var totalTipHeight = $("#flotTip").outerHeight() + this.tooltipOptions.shifts.y;
+        if ((pos.x - $(window).scrollLeft()) > ($(window).innerWidth() - totalTipWidth)) {
+            pos.x -= totalTipWidth;
+        }
+        if ((pos.y - $(window).scrollTop()) > ($(window).innerHeight() - totalTipHeight)) {
+            pos.y -= totalTipHeight;
+        }
         this.tipPosition.x = pos.x;
         this.tipPosition.y = pos.y;
     };
-    
+
     /**
      * core function, create tooltip content
      * @param  {string} content - template with tooltip content
@@ -140,22 +158,22 @@
      * @return {string} real tooltip content for current item
      */
     FlotTooltip.prototype.stringFormat = function(content, item) {
-        
+
         var percentPattern = /%p\.{0,1}(\d{0,})/;
         var seriesPattern = /%s/;
-        var xPattern = /%x\.{0,1}(\d{0,})/;
-        var yPattern = /%y\.{0,1}(\d{0,})/;
+        var xPattern = /%x\.{0,1}(?:\d{0,})/;
+        var yPattern = /%y\.{0,1}(?:\d{0,})/;
 
         // if it is a function callback get the content string
         if( typeof(content) === 'function' ) {
-            content = content(item.series.data[item.dataIndex][0], item.series.data[item.dataIndex][1]);
+            content = content(item.series.label, item.series.data[item.dataIndex][0], item.series.data[item.dataIndex][1], item);
         }
 
         // percent match for pie charts
         if( typeof (item.series.percent) !== 'undefined' ) {
             content = this.adjustValPrecision(percentPattern, content, item.series.percent);
         }
-        
+
         // series match
         if( typeof(item.series.label) !== 'undefined' ) {
             content = content.replace(seriesPattern, item.series.label);
@@ -202,35 +220,32 @@
         return (typeof this.tooltipOptions.yDateFormat !== 'undefined' && this.tooltipOptions.yDateFormat !== null);
     };
 
-    // 
+    //
     FlotTooltip.prototype.timestampToDate = function(tmst, dateFormat) {
         var theDate = new Date(tmst);
         return $.plot.formatDate(theDate, dateFormat);
     };
-    
-    // 
+
+    //
     FlotTooltip.prototype.adjustValPrecision = function(pattern, content, value) {
-        
+
         var precision;
-        if( content.match(pattern) !== null ) {
+        var matchResult = content.match(pattern);
+        if( matchResult !== null ) {
             if(RegExp.$1 !== '') {
                 precision = RegExp.$1;
                 value = value.toFixed(precision);
 
-                // only replace content if precision exists
+                // only replace content if precision exists, in other case use thickformater
                 content = content.replace(pattern, value);
             }
         }
-    
         return content;
     };
-    
-    // to have controll over existing instances
-    var tooltipsCollection = [];
 
-    // 
+    //
     var init = function(plot) {
-        tooltipsCollection.push( new FlotTooltip(plot) );
+      new FlotTooltip(plot);
     };
 
     // define Flot plugin
@@ -238,7 +253,7 @@
         init: init,
         options: defaultOptions,
         name: 'tooltip',
-        version: '0.6'
+        version: '0.6.1'
     });
 
 })(jQuery);
